e4ecf63b089ffa50a04d12a36a3417e9316a3992
phucvo <petphuc.vq@gmail.com>
2024-11-18 13:10:13 +0700

    Handling exceptions

--- src/main/java/librarymanagement/exception/GlobalExceptionHandler.java
+++ src/main/java/librarymanagement/exception/GlobalExceptionHandler.java
@@ -1,4 +1,5 @@
 package librarymanagement.exception;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,6 +11,19 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    // Handle DataIntegrityViolationException (Lỗi ràng buộc CSDL)
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        String message = "Invalid input: " + ex.getMostSpecificCause().getMessage();
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
+
+    // Xử lý tất cả lỗi BAD_REQUEST chung
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>("Invalid request: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
